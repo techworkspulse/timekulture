432efa94b75c3e5ddc11d666771db69555ab4916
dsyenket <yenket@digitalsymphony.it>
2018-07-09 11:39:35 +0800

    Signed-off-by: dsyenket <yenket@digitalsymphony.it>

--- resources/views/bonus.blade.php
+++ resources/views/bonus.blade.php
@@ -711,7 +711,7 @@
 
                                     <div class="col-6 col-sm-6  visible-xs visible-sm"> 
 
-                                        <a href="#instagram-save" data-toggle="modal" ><img src="{{ url('assets/img/tk/instagram-logo.svg') }}" class="img-responsive center-block social-share"></a>
+                                        <a id="instagramshare-btn" href="#instagram-save" data-toggle="modal" ><img src="{{ url('assets/img/tk/instagram-logo.svg') }}" class="img-responsive center-block social-share"></a>
 
                                     </div>
 
@@ -841,6 +841,15 @@
 @section('customJS')
 <script type="text/javascript">
     $(document).ready(function() {
+        $('#submit-share').addClass('disabled');
+
+        $('#facebookshare-btn').click(function() {
+            $('#submit-share').removeClass('disabled');
+        });
+
+        $('#instagramshare-btn').click(function() {
+            $('#submit-share').removeClass('disabled');
+        })
     });
 </script>
 @endsection
