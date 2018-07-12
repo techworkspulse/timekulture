fdc2007af0923d8a0bd41f7d518c0ff0a4c59241
dsyenket <yenket@digitalsymphony.it>
2018-07-12 17:52:34 +0800

    Signed-off-by: dsyenket <yenket@digitalsymphony.it>

--- app/Console/Commands/GenerateDailyWinners.php
+++ app/Console/Commands/GenerateDailyWinners.php
@@ -54,7 +54,7 @@ class GenerateDailyWinners extends Command
 
         $match = Match::selectRaw('id, MAX(points) AS point, player_id')
             ->whereNotIn('player_id',$winnerList)
-            ->where('point','<=','1300')
+            ->where('matches.points','<=','1300')
 			->where('completion_status',1)
             ->whereBetween('created_at', [date('Y-m-d 00:00:00', strtotime(date('Y-m-d 00:00:00'). ' -1 day')),date('Y-m-d 00:00:00')])
             ->groupBy('player_id')->orderBy('point','desc')->take(10)->get();    
